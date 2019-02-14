45ad13f25615446250f621ff5057cc008e020f3e
dannyt <cobreti_@hotmail.com>
2019-02-13 20:23:36 -0500

    added index to prevent duplicate usernames

--- Services/MongoDbService.cs
+++ Services/MongoDbService.cs
@@ -33,23 +33,56 @@ namespace authService.Services
                 var mongoClient = new MongoClient(settings);
 
                 var db = mongoClient.GetDatabase(AppSettings.Connections.MongoDb.Database);
-                var collection = db.GetCollection<User>("authUsers");
 
+                var collections = db.ListCollections().ToList();
+                if (collections.Count == 0)
+                {
+                    var usersCollection = await CreateAuthUsersCollection(db);
+                    await CreateDefautUser(usersCollection);
+                }
+                
+//                var users = db.GetCollection<User>("authUsers");
+//
+//                var result = collection.Find(x => x.Name == "Admin").ToList();
+                
                 // var doc = new BsonDocument();
                 // doc.Add("Name", "Admin");
                 // doc.Add("Password", "Admin@123");
                 
-                await collection.InsertOneAsync(new User()
-                {
-                    Name = "Admin",
-                    Password = "Admin@123"
-                });
 
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
+            }
+        }
+
+        async Task<IMongoCollection<User>> CreateAuthUsersCollection(IMongoDatabase db)
+        {
+            db.CreateCollection("authUsers");
+            var collection = db.GetCollection<User>("authUsers");
+            var options = new CreateIndexOptions() { Unique = true };
+            var field = new StringFieldDefinition<User>("Name");
+            var indexDefinition = new IndexKeysDefinitionBuilder<User>().Ascending(field);
+            await collection.Indexes.CreateOneAsync(indexDefinition, options);
+
+            return collection;
+        }
+
+        async Task CreateDefautUser(IMongoCollection<User> usersCollection)
+        {
+            try
+            {
+                await usersCollection.InsertOneAsync(new User()
+                {
+                    Name = "Admin",
+                    Password = "Admin@123"
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
